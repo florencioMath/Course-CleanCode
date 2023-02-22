fc05de1d57ea45956a0b5f31748dcd20f45635d7
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-22 20:55:00 -0300

    feat: My solution to challenge Regras em condicionais.

--- challenges/05-regras-em-condicionais.ts
+++ challenges/05-regras-em-condicionais.ts
@@ -1,21 +1,27 @@
 // Regras em condicionais
 
-const necessaryGradeToBeApproved = 7
-const studentGrade = 10
+const necessaryGradeToBeApproved = 7;
+const studentGrade = 10;
 
-const numberOfAbsensesToFailSchool = 100
-const studentNumberOfAbsenses = 109
+const numberOfAbsensesToFailSchool = 100;
+const studentNumberOfAbsenses = 109;
 
+// separeted the if's and put a general return.
 function checkIfStudentPassedTheSchoolYear() {
   if (studentGrade < necessaryGradeToBeApproved) {
-    return { error: true, message: 'Student was not approved because his grade was below the necessary.'}
-  } else {
-    if(studentNumberOfAbsenses > numberOfAbsensesToFailSchool) {
-      return { error: true, message: 'Student was not approved because of his absenses'}
-    } else {
-      return { error: false, message: 'Student was approved :)'}
-    }
+    return {
+      error: true,
+      message:
+        'Student was not approved because his grade was below the necessary.',
+    };
   }
+  if (studentNumberOfAbsenses > numberOfAbsensesToFailSchool) {
+    return {
+      error: true,
+      message: 'Student was not approved because of his absenses',
+    };
+  }
+  return { error: false, message: 'Student was approved :)' };
 }
 
-console.log(checkIfStudentPassedTheSchoolYear())
+console.log(checkIfStudentPassedTheSchoolYear());
