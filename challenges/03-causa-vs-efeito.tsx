039733bfec637d5baef9d0c2c677a174084adcee
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-21 21:59:40 -0300

    feat: My solution to Causa vs Efeito challenge.

--- challenges/03-causa-vs-efeito.tsx
+++ challenges/03-causa-vs-efeito.tsx
@@ -18,24 +18,25 @@ function fetchUser() {
 }
 
 export function UserProfile() {
-  const [shouldNotRenderUserName, setShouldNotRenderUserName] = useState(false);
+  // shouldNotRenderUserName to isUserNameRendering
+  const [isUserNameRendering, setIsUserNameRendering] = useState(false);
   const [userData, setUserData] = useState<User>();
 
   useEffect(() => {
     function loadUser() {
-      setShouldNotRenderUserName(true);
+      setIsUserNameRendering(true);
 
       const fetchUserResponse = fetchUser();
 
       setUserData(fetchUserResponse.data.user);
 
-      setShouldNotRenderUserName(false);
+      setIsUserNameRendering(false);
     }
 
     loadUser();
   });
 
-  if (shouldNotRenderUserName) {
+  if (isUserNameRendering) {
     return <p>Loading...</p>;
   }
 
