7e742a100bd98daca56891067657af4efe64cf20
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-23 20:40:59 -0300

    feat: React tips overview.

--- react/react-tips.tsx
+++ react/react-tips.tsx
@@ -1,1 +1,19 @@
 // Dicas e boas práticas de Clean Code no React
+
+/*
+Foi abordadoos pontos:
+- Desacoplando componentes
+
+- Componentes puros
+
+- Funções e eventos no React
+
+- Composição vs Customização/Configuração
+
+- Condicionais no render
+
+Passando um entendimento em como componentizar e melhorar a estrutura dos compoenetes para ter um código
+mais perfomatico e de fácil manutenção.
+*/
+
+// Encontre mais cursos da Rocketseat aqui: https://lp.rocketseat.com.br/programas-rocketseat
