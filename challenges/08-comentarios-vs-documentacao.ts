bcd5fce2152c4ed85dea139e71b53b31ebd962d3
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-22 21:46:08 -0300

    feat: My solution to  challenge Comentários vs Documentação.

--- challenges/08-comentarios-vs-documentacao.ts
+++ challenges/08-comentarios-vs-documentacao.ts
@@ -1,20 +1,15 @@
 // Comentários vs Documentação
 
-// Essa função tem o objetivo de registrar um novo usuário no banco.
+// left only one commentary which tells why the code was made the way it was
 async function register(data) {
-  // Desestrutura os dados recebidos.
   const { email, name, avatar } = data
 
-  // Verifica se o avatar foi informado.
   if (!avatar) return { error: 'avatar is required' }
 
-  // Verifica se o nome foi informado.
   if(!name) return { error: 'name is required' }
 
-  // Essa função retorna o usuário a partir do email informado
   const userMail = getUserByEmail(email)
 
-  // Verificação se o email informado já foi utilizado por algum usuário.
   if (userMail) {
     return { error: 'email already used' }
   }
@@ -23,7 +18,6 @@ async function register(data) {
   // Mais informações na issue https://github.com/rocketseat-education/example-repository/issues/1
   const avatar2 = convertImageToJPG(avatar)
 
-  // Cria o usuário no banco de dados
   const user = await createUser({ email, name, avatar: avatar2 })
 
   return { user }
