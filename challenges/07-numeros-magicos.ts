6dc55eec48c0a3c52301d049e2bdb2063ac76854
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-22 21:38:13 -0300

    feat: My solution to Números mágicos.

--- challenges/07-numeros-magicos.ts
+++ challenges/07-numeros-magicos.ts
@@ -1,7 +1,12 @@
 // Números mágicos
 
+const INTERVAL_30_MINUTES = 60 * 30 * 1000;
+
 function lookForUpdades() {}
 
-setInterval(lookForUpdades, 60 * 30 * 1000)
+// created a variable named with the amount of time and also send one with a comment
+setInterval(lookForUpdades, 60 * 30 * 1000) // 30 minutes
+setInterval(lookForUpdades, INTERVAL_30_MINUTES)
 
-function calculateDiscount(price, discount) {}
+//  changed variables names to show the currency and percentage
+function calculateDiscount(priceInCents, discountInPercentage) {}
