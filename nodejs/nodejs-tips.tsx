9cb78acf17433a32f69148b35824d3a246e2d521
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-23 22:28:53 -0300

    feat: Nodejs tips overview.

--- nodejs/nodejs-tips.tsx
+++ nodejs/nodejs-tips.tsx
@@ -1,1 +1,13 @@
 // Dicas e boas práticas de Clean Code no Node.js
+
+/*
+Foi abordadoos os  princícios de:
+- SOLID
+
+- DDD
+
+Passando um entendimento em como melhorar a estrutura do back-end para ter um código
+mais perfomatico e de fácil manutenção.
+*/
+
+// Encontre mais cursos da Rocketseat aqui: https://lp.rocketseat.com.br/programas-rocketseat
